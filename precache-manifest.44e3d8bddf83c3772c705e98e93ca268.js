5a48439ad5e96f9a405cf4696a0feb1b815b1017
saurav <avisaurav485@gmail.com>
2021-05-07 12:31:29 +0530

    Updates

---
+++ precache-manifest.44e3d8bddf83c3772c705e98e93ca268.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393849a37d6d2641c250020a9c9bf934",
+    "url": "/registration-page/index.html"
+  },
+  {
+    "revision": "f19b9b8d60c077b52f37",
+    "url": "/registration-page/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f19b9b8d60c077b52f37",
+    "url": "/registration-page/static/js/2.21da9cdd.chunk.js"
+  },
+  {
+    "revision": "226c74e9bd2d4b7701cb",
+    "url": "/registration-page/static/js/main.08877148.chunk.js"
+  },
+  {
+    "revision": "c8ff96c4935959a254ca",
+    "url": "/registration-page/static/js/runtime~main.81d0a001.js"
+  }
+]);
